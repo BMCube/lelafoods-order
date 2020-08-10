4beb0aca206806c3cb8126df8d4a06a67438e4f2
Biruk Bekele <biruk.bekele@gmail.com>
2020-08-10 00:33:58 -0500

    food controller

---
+++ src/main/java/edu/miu/lelafoods/order/controller/FoodController.java
@@ -0,0 +1,51 @@
+package edu.miu.lelafoods.order.controller;
+import edu.miu.lelafoods.order.service.FoodService;
+import edu.miu.lelafoods.order.domain.Food;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
+public class FoodController {
+
+    @Autowired
+    FoodService foodService;
+
+    @GetMapping({"", "/all"})
+    public List<Food> findAll() {
+        List<Food> foodsList = foodService.findall();
+        return foodsList;
+
+    }
+
+
+    @GetMapping("/{id}")
+    public Food findFoodById(@PathVariable("id") Long id) {
+
+        return foodService.findById(id);
+    }
+
+    @GetMapping("/{name}")
+    public Food findFoodByName(@PathVariable("name") String name) {
+
+        return foodService.findByFoodName(name);
+    }
+
+
+    @PostMapping("")
+    public void addFood(@RequestBody Food food) {
+        foodService.save(food);
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        foodService.deleteById(id);
+
+    }
+
+
+}
