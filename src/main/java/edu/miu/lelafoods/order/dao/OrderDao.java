930ed807a5c41b04f9058f496cb45a8df1c64a80
Biruk Bekele <biruk.bekele@gmail.com>
2020-08-09 21:18:22 -0500

    dao implementation

---
+++ src/main/java/edu/miu/lelafoods/order/dao/OrderDao.java
@@ -0,0 +1,10 @@
+package edu.miu.lelafoods.order.dao;
+
+import edu.miu.lelafoods.order.domain.Order;
+
+import java.util.List;
+
+public interface OrderDao  extends  GenericDao<Order> {
+
+    public List<Order> showOrdersForToday();
+}
