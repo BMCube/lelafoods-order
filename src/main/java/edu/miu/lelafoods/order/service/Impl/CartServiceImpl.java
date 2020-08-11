fed10041190d01cbb34821661f5deb2a68ed525e
Biruk Bekele <biruk.bekele@gmail.com>
2020-08-11 09:44:41 -0500

    cart module, controller,dao,service

---
+++ src/main/java/edu/miu/lelafoods/order/service/Impl/CartServiceImpl.java
@@ -0,0 +1,63 @@
+package edu.miu.lelafoods.order.service.impl;
+
+import edu.miu.lelafoods.order.dao.CartDao;
+import edu.miu.lelafoods.order.dao.FoodDao;
+import edu.miu.lelafoods.order.dao.OrderDao;
+import edu.miu.lelafoods.order.domain.Cart;
+import edu.miu.lelafoods.order.domain.Food;
+import edu.miu.lelafoods.order.domain.Order;
+import edu.miu.lelafoods.order.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class CartServiceImpl implements CartService {
+    @Autowired
+    CartDao cartDao;
+    @Autowired
+    OrderDao orderDao;
+    @Autowired
+    FoodDao foodDao;
+    @Override
+    public void save(Cart cart) {
+        cartDao.save(cart);
+    }
+
+    @Override
+    public void addToCart(Long idCart, Long idFood, Integer quantity) {
+        Cart cart = cartDao.findOne(idCart);
+        Food food = foodDao.findOne(idFood);
+        cart.getOrder().add(new Order(quantity, food));
+        cartDao.update(cart);
+    }
+
+    @Override
+    public Long ordered(Long idCart) {
+        return null;
+    }
+
+    @Override
+    public List<Cart> findall() {
+        return cartDao.findAll();
+    }
+
+    @Override
+    public void deleteCart(long id) {
+        cartDao.deleteById(id);
+
+
+    }
+
+
+         /*Order Order = this.findItemInCart(productCode);
+
+        if (itemInfo != null) {
+            this.cartItem.remove(itemInfo);*/
+
+        }
+
+
