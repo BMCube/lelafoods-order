dc96c04bf8ef12d4063e4ee17386836d448c9b69
Mikiyas Belhu <mikiyasbelhu@gmail.com>
2020-08-12 15:21:45 -0500

    add pojo classes

---
+++ src/main/java/edu/miu/lelafoods/order/domain/Restaurant.java
@@ -0,0 +1,49 @@
+package edu.miu.lelafoods.order.domain;
+
+import java.util.List;
+
+public class Restaurant {
+
+    private Long id;
+
+    private String name;
+
+    List<Address> addresses;
+
+    List<Food> foods;
+
+    public Restaurant() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public List<Food> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(List<Food> foods) {
+        this.foods = foods;
+    }
+}
