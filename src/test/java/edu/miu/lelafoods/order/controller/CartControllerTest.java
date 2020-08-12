ef530af72ba59e8cc2b4e9a60de638eba8c0504c
mickiasmek <mickias6@gmail.com>
2020-08-11 20:17:14 -0500

    add update cart controller test

--- src/test/java/edu/miu/lelafoods/order/controller/CartControllerTest.java
+++ src/test/java/edu/miu/lelafoods/order/controller/CartControllerTest.java
@@ -1,7 +1,12 @@
 package edu.miu.lelafoods.order.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.miu.lelafoods.order.domain.Cart;
+import edu.miu.lelafoods.order.domain.Food;
+import edu.miu.lelafoods.order.domain.Order;
+import edu.miu.lelafoods.order.domain.OrderStatus;
 import edu.miu.lelafoods.order.service.CartService;
+import edu.miu.lelafoods.order.service.OrderService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +17,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,4 +63,67 @@ public class CartControllerTest {
                 .andExpect(status().isOk())
                 .andReturn();
     }
+
+  /*  @Test
+    public void testAddOrdertoCart() throws Exception{
+        Food myFood = new Food();
+        myFood.setName("Pasta");
+        myFood.setPrice(20.0);
+        myFood.setId(1L);
+        Cart cart = new Cart();
+        cart.setId(1L);
+        Order order = new Order();
+        order.setId(1L);
+        order.setOrderAmount(2);
+        order.setFood(myFood);
+
+        String jsonRequest = om.writeValueAsString(myFood.getId(),cart.getId(),order.getOrderAmount());
+        when(cartService.addToCart(1l, 1l,2)).thenReturn();
+        mockMvc.perform(put(CartController.BASE_URL+"/addTocart/1").contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        assertEquals(content, jsonRequest);
+
+
+    }*/
+
+
+   /* @Test
+    public void testCreateCart() throws Exception{
+        Food food = new Food();
+        food.setId(1L);
+        food.setPrice(13.5);
+        food.setName("Tofu");
+        Order myOrder = new Order();
+        myOrder.setId(1L);
+        Date myDate = new Date();
+        myOrder.setOrderedDate(myDate);
+        myOrder.setOrderQuantity(3);
+        myOrder.setFood(food);
+        myOrder.setStatus(OrderStatus.NEW.toString());
+        List<Order> order = new ArrayList<>();
+        order.add(myOrder);
+        Cart cart = new Cart();
+        cart.setId(1L);
+        cart.setOrder(order);
+        System.out.println(cart.getOrder().get(0).getOrderQuantity());
+
+        String inputJson = om.writeValueAsString(cart);
+        //when(cartService.save(cart)).thenReturn(cart);
+        MvcResult result = mockMvc.perform(post(CartController.BASE_URL)
+        .content(inputJson).contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(1)))
+
+                .andReturn();
+     String content = result.getResponse().getContentAsString();
+     assertEquals(content, inputJson);
+
+
+    }*/
+
+
+
 }
